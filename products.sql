1f20f891635fdf62c1a0dfb286d46c5b53dbf926
Neil <neilkosh001@gmail.com>
2018-08-07 19:31:01 -0400

    connected mysql sever

---
+++ products.sql
@@ -0,0 +1,8 @@
+INSERT INTO products (ProductName, DepartmentName, Price, StockSquantity)
+
+VALUES ("Uncharted 4", "Video Games", 49.59, 150),
+("Play Station 2", "Video Games" , 50.00 , 20),
+("Tomb Raider", "Video Games", 34.00, 50),
+("Mad Max", "Video Games", 49.95, 120);
+
+SELECT * FROM bamazon.products;
---
+++ schema.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE bamazon;
+
+USE bamazon;
+
+CREATE TABLE products (
+    item_id INTEGER AUTO_INCREMENT NOT NULL,
+    product_name VARCHAR (45) NOT NULL,
+    department_name VARCHAR (45) NOT NULL,
+    price decimal (10,4) NOT NULL,
+    PRIMARY KEY (item_id)
+
+);
+
